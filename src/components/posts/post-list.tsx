26505735dc05696bb6e37cb58dfd82cf9a6c4c22
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2024-04-17 14:07:12 +0600

    add reusable post life

--- src/components/posts/post-list.tsx
+++ src/components/posts/post-list.tsx
@@ -1,9 +1,16 @@
+import type { PostWithData } from '@/db/queries/posts';
 import type { Post, User, Topic } from '@prisma/client';
 import Link from 'next/link';
 import paths from '@/paths';
 
+interface PostListProps {
+  fetchData: () => Promise<PostWithData[]>;
+}
+
 // TODO: Get list of posts into this component somehow
-export default function PostList() {
+export default async function PostList({ fetchData }: PostListProps) {
+  const posts = await fetchData();
+
   const renderedPosts = posts.map((post) => {
     const topicSlug = post.topic.slug;
 
@@ -17,9 +24,7 @@ export default function PostList() {
           <h3 className="text-lg font-bold">{post.title}</h3>
           <div className="flex flex-row gap-8">
             <p className="text-xs text-gray-400">By {post.user.name}</p>
-            <p className="text-xs text-gray-400">
-              {post._count.comments} comments
-            </p>
+            <p className="text-xs text-gray-400">{post._count.comments} comments</p>
           </div>
         </Link>
       </div>
